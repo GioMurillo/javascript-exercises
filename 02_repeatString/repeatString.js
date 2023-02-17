e4521a522dca097052dd05f3c5a91363e2dcf629
GioMurillo <giovanni.p.murillo@gmail.com>
2023-02-17 06:40:54 -0800

    Pass first test. Remove .skip

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatQuantity) {
+  let i = 0;
+  let outputString = '';
+  do {
+    outputString += inputString;
+    i++;
+  }
+  while (i < repeatQuantity);
 
+  return outputString;
 };
 
 // Do not edit below this line
