aa29f9ca8d8466c03d1a43d9bca5bc4d709f4468
GioMurillo <giovanni.p.murillo@gmail.com>
2023-02-25 07:23:11 -0800

    Reverse a single word with reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+let backwardString = '';
+const reverseString = function(inputString) {
+  let i = inputString.length - 1;
+  do {
+    backwardString += inputString[i];
+    i--;
+  } while (i>=0);
+  return backwardString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
