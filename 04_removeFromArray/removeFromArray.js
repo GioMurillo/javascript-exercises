c1d80a035138fde7093be47cc6be5739ba222f1e
GioMurillo <giovanni.p.murillo@gmail.com>
2023-06-21 22:08:57 -0700

    Attempt to code after 4 months. Strings don't work this way.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputString, removeValue) {
+  let outputString='';
+  let j=0;
+  for (i=0;i<inputString.length;i++) {
+    if (inputString[i]!=removeValue) {
+      outputString[i]=inputString[i];
+      j++;
+    } else {
+      j++;
+    }
+  }
+  return outputString;
 };
 
 // Do not edit below this line
