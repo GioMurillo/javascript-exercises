3aa8f3ebab2a85288a55d89e00d9f48eecf1a41f
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-08 08:56:08 -0700

    Still doesnt work but saving progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,15 +2,17 @@ const removeFromArray = function(originalArray, ...args) {
   let inputArray=[];
   let outputArray=[];
   outputArray = args;
-  for (i=0;i<inputArray.length-1;i++) {
+  console.log(inputArray);
+  console.log(originalArray);
+  //using loops for now, but can go back and change to forEach() later
+  for (i=0;i<originalArray.length-1;i++) {
     for (j=0;j<args.length-1;j++){
-      if (inputArray[i]!=args[j]) {
-        outputArray+=inputArray[i];
+      if (originalArray[i]!=args[j]) {
+        outputArray.push(args[j]);
       } 
     }
-    
   }
-  return outputString;
+  return outputArray;
 };
 
 // Do not edit below this line
