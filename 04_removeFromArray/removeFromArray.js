81f22470f620c02b06c1beaaa4c4492d0482b815
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-18 06:08:12 -0700

    Change loop conditions. Solve first test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,15 @@
 const removeFromArray = function(originalArray, ...args) {
-  let inputArray=[];
   let outputArray=[];
-  outputArray = args;
-  console.log(inputArray);
-  console.log(originalArray);
   //using loops for now, but can go back and change to forEach() later
-  for (i=0;i<originalArray.length-1;i++) {
-    for (j=0;j<args.length-1;j++){
+  for (let i=0; i<originalArray.length; i++) {
+    for (let j=0; j<args.length; j++){
       if (originalArray[i]!=args[j]) {
-        outputArray.push(args[j]);
-      } 
+        outputArray.push(originalArray[i]);
+      }
+      console.log("outputArray= " + outputArray); 
     }
   }
+  console.log("outputArray after loops = " + outputArray);
   return outputArray;
 };
 
