8d5c90a09b030b81d34c1d76b7fb8e2d0b0e3f28
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-18 12:09:11 -0700

    Revert to code that passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,13 +3,11 @@ const removeFromArray = function(originalArray, ...args) {
   //using loops for now, but can go back and change to forEach() later
   for (let i=0; i<originalArray.length; i++) {
     for (let j=0; j<args.length; j++){
-      if (originalArray[i]===args[j]) {
-        j++;
+      if (originalArray[i]!=args[j]) {
+        outputArray.push(originalArray[i]);
       }
-      console.log(originalArray[i] + " and " + args[j]);
+      console.log("outputArray= " + outputArray); 
     }
-    outputArray.push(originalArray[i]);
-    console.log("outputArray= " + outputArray); 
   }
   console.log("outputArray after loops = " + outputArray);
   return outputArray;
