54cbf3130f04c220a857e9724681f8e29edf663d
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-18 12:12:31 -0700

    Realize github has a history funcion and thats the whole point of git

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,11 +3,13 @@ const removeFromArray = function(originalArray, ...args) {
   //using loops for now, but can go back and change to forEach() later
   for (let i=0; i<originalArray.length; i++) {
     for (let j=0; j<args.length; j++){
-      if (originalArray[i]!=args[j]) {
-        outputArray.push(originalArray[i]);
+      if (originalArray[i]===args[j]) {
+        j++;
       }
-      console.log("outputArray= " + outputArray); 
+      console.log(originalArray[i] + " and " + args[j]);
     }
+    outputArray.push(originalArray[i]);
+    console.log("outputArray= " + outputArray); 
   }
   console.log("outputArray after loops = " + outputArray);
   return outputArray;
