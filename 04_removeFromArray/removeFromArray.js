66b1a6e08f72dfb81c578a4d364e04030320deae
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-19 05:54:33 -0700

    Create argsArray from args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,15 @@
 const removeFromArray = function(originalArray, ...args) {
   let outputArray=[];
+  let argsArray=Array.from(args);
   //using loops for now, but can go back and change to forEach() later
   for (let i=0; i<originalArray.length; i++) {
-    for (let j=0; j<args.length; j++){
-      if (originalArray[i]===args[j]) {
+    for (let j=0; j<argsArray.length; j++){
+      if (originalArray[i]===argsArray[j]) {
         i++;
-        console.log("Found an element to skip:" + args[j]);
+        console.log("Found an element to skip:" + argsArray[j]);
       }
       console.log("Comparing " + originalArray[i] + "["+i+"]" + " and " 
-        + args[j] + "["+j+"]");
+        + argsArray[j] + "["+j+"]");
     }
     outputArray.push(originalArray[i]);
     console.log("outputArray= " + outputArray); 
