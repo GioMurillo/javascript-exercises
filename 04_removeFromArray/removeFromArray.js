423bf47667fd7f8140e08b66c735dcc6b703f3eb
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-27 22:02:41 -0700

    Try changing logic to use forEach It is not immediatel clear to me how I would do this without creating a mess

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,26 +1,20 @@
 const removeFromArray = function(originalArray, ...args) {
-  let outputArray=[];
-  let argsArray=Array.from(args);
-  outputArray = originalArray;
-  //using loops for now, but can go back and change to forEach() later
-  for (let i=0; i<originalArray.length; i++) {
-    for (let j=0; j<argsArray.length; j++){
-      if (originalArray[i]===argsArray[j]) {
-        console.log("Comparing " + originalArray[i] + "["+i+"]" + " and " 
-        + argsArray[j] + "["+j+"]");
-        console.log("Found an element to remove:" + argsArray[j]);
-        outputArray.splice(i,1);
-        console.log("Deprecate i");
-        i--;
-      }
-      console.log("Comparing " + originalArray[i] + "["+i+"]" + " and " 
-        + argsArray[j] + "["+j+"]");
-    }
-    console.log("outputArray= " + outputArray); 
-  }
-  console.log("outputArray after loops = " + outputArray);
-  return outputArray;
+  //originalArray.forEach("originalArray: " + logArrayElements);
+  //args.forEach(logArrayElements);
+
+  outputArray = [];
+  valuesToRemove = Array.from(args); //go from array-like to array
+
+
 };
+
+const logArrayElements = (element, index /*, array */) => {
+  console.log(`a[${index}] = ${element}`);
+};
+
+function checkMatch(element, index, array) {
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
