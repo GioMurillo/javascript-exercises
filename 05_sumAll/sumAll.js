899a401ce9fe0a719cdf680930ce12fb6aba722c
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-31 18:32:22 -0700

    Catch invalid parameters First attempt at suming all numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  var sum = 0;
+  if(num1.typeof != 'number' || num2.typeof != 'number'){
+    return 'ERROR';
+  } else if (num1 < 0 || num2 <0) {
+    return 'ERROR';
+  } else {
+    if (num1 == num2) {
+      return 0;
+    } else if (num1 > num2) {
+        for(i = num2; i < num1; i++) {
+          sum += i;
+        }
+    } else {
+       for(i=num1; i< num2; i++) {
+         sum += 1;
+       }
+    }
+  }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
