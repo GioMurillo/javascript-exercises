2b68dae8e2623779f8396a6fc9ee28678f3d09a2
GioMurillo <giovanni.p.murillo@gmail.com>
2023-07-31 19:01:44 -0700

    Fix various errors Revise typeof Add return sum Revise loop starting and end point

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,25 @@
 const sumAll = function(num1, num2) {
   var sum = 0;
-  if(num1.typeof != 'number' || num2.typeof != 'number'){
+
+  if(typeof num1 != 'number' || typeof num2 != 'number'){
     return 'ERROR';
-  } else if (num1 < 0 || num2 <0) {
+  } else if (num1<0 || num2<0) {
     return 'ERROR';
   } else {
     if (num1 == num2) {
       return 0;
     } else if (num1 > num2) {
-        for(i = num2; i < num1; i++) {
+        sum = num2;
+        for(i=num2+1; i<num1+1; i++) {
           sum += i;
         }
     } else {
-       for(i=num1; i< num2; i++) {
-         sum += 1;
+       sum = num1;
+       for(i=num1+1; i<num2+1; i++) {
+        sum += i;
        }
     }
+    return sum;
   }
 };
 
