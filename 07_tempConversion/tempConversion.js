b648ee3f2154eac439e05430378acc3d4de2b603
GioMurillo <giovanni.p.murillo@gmail.com>
2023-08-02 19:01:45 -0700

    Convert to Celsius with rounding and decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,16 @@
 const convertToCelsius = function(fTemp) {
-  return (fTemp - 32)*5/9;
+  const cTemp = (fTemp - 32)*5/9
+  function round(num, decimalPlaces = 0) {
+    var p = Math.pow(10, decimalPlaces);
+    var n = (num * p) * (1 + Number.EPSILON);
+    return Math.round(n) / p;
+  }
+  if(cTemp - Math.floor(cTemp) !== 0){
+    return round(cTemp,1);
+  } else {
+    return round(cTemp);
+  }
+
 };
 
 const convertToFahrenheit = function() {
